8e02a52c0ae5e1a88f21ecbd1ea55b72e64ec951
Sveta Kryukova <kryukova.sv.13@gmail.com>
2023-08-07 13:51:43 +0300

    fixes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/Sveta-Kryukova/vue-app',
+  configureWebpack: {
+    plugins: [new MiniCssExtractPlugin()],
+  },
 })
